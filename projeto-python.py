9b28e3d3f14ac2eff6e8ab797bbdf24c88f6e847
ariellucaslf <142851546+ariellucaslf@users.noreply.github.com>
2024-05-20 23:54:37 -0300

    Create projeto-python.py
    primeiro commit, testando o codigo no repositorio

---
+++ projeto-python.py
@@ -0,0 +1,16 @@
+#comecando o teste 
+
+login = []
+senha = []
+
+id = input('digite seu id: ')
+password = input('digite a sua senha numerica: ')
+
+login.append('')
+senha.append('')
+
+
+if id and password == login and senha:
+    print('Login aprovado !')
+else:
+    print('Id ou Senha Incorretos.')
