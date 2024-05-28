6e712c65d3617499a8b61cebf4b3956fff28fe53
AnnandaV <annandavictoria16@gmail.com>
2024-05-28 16:54:35 -0300

    dividindo janela

--- projeto-python.py
+++ projeto-python.py
@@ -13,4 +13,10 @@ janela.geometry("310x300")
 janela.configure(background=cor2)
 janela.resizable(width=FALSE, height=FALSE)
 
+frame_cima = Frame(janela, width=310, height=50, bg=cor2, relief="flat")
+frame_cima.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
+
+frame_baixo = Frame(janela, width=310, height=250, bg=cor2, relief="flat")
+frame_baixo.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
 janela.mainloop()
