439ecca1ae724ec55ccdddfe21b9bebc7474f077
AnnandaV <annandavictoria16@gmail.com>
2024-05-28 16:43:18 -0300

    criando janela

--- projeto-python.py
+++ projeto-python.py
@@ -6,3 +6,11 @@ cor2 = "#feffff" # branco
 cor3 = "#249c89" # verde
 cor4 = "#38576b" # azul escuro (valor)
 cor5 = "#403d3d" # cinza (letra)
+
+janela = Tk()
+janela.title("")
+janela.geometry("310x300")
+janela.configure(background=cor2)
+janela.resizable(width=FALSE, height=FALSE)
+
+janela.mainloop()
